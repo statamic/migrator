3fdb04ea07df65c935c85258651b468aac596597
Jesse Leite <jesseleite@gmail.com>
2019-09-06 17:23:39 -0400

    Colonel not needed.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,7 +6,6 @@ class TestCase extends \Orchestra\Testbench\TestCase
 {
     protected function setUp(): void
     {
-        require_once(__DIR__.'/ConsoleKernel.php');
         require_once(__DIR__.'/ExceptionHandler.php');
 
         parent::setUp();
