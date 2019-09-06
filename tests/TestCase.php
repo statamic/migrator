df770c5126105124f00a948907f1c88039c31669
Jesse Leite <jesseleite@gmail.com>
2019-09-06 16:30:08 -0400

    Port migrate fieldset test.

---
+++ tests/TestCase.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests;
+
+class TestCase extends \Orchestra\Testbench\TestCase
+{
+    protected function setUp(): void
+    {
+        require_once(__DIR__.'/ConsoleKernel.php');
+        require_once(__DIR__.'/ExceptionHandler.php');
+
+        parent::setUp();
+    }
+
+    protected function getPackageProviders($app)
+    {
+        return [
+            \Statamic\Providers\StatamicServiceProvider::class,
+            \Statamic\Migrator\ServiceProvider::class
+        ];
+    }
+
+    protected function getPackageAliases($app)
+    {
+        return ['Statamic' => 'Statamic\Statamic'];
+    }
+}
