32d660c5493c431e18204d2fd3bdf1c231d213ed
Jesse Leite <jesseleite@gmail.com>
2019-09-19 17:49:08 -0400

    Migrate collection logic (WIP).

---
+++ src/Migrators/CollectionMigrator.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Statamic\Migrator\Migrators;
+
+use Statamic\Facades\YAML;
+
+class CollectionMigrator extends Migrator
+{
+    /**
+     * Migrate file.
+     *
+     * @param string $handle
+     */
+    public function migrate($handle)
+    {
+        $this->handle = $handle;
+        $this->newPath = base_path("content/collections/{$handle}");
+
+        $this
+            ->validateUnique()
+            ->migrateYamlConfig();
+    }
+
+    /**
+     * Migrate yaml config.
+     *
+     * @return $this
+     */
+    protected function migrateYamlConfig()
+    {
+        $config = collect(YAML::parse($this->files->get($this->newPath('folder.yaml'))));
+
+        $config->put('blueprints', [$config->get('fieldset')]);
+        $config->forget('fieldset');
+
+        $this->files->put($this->newPath("../{$this->handle}.yaml"), YAML::dump($config));
+        $this->files->delete($this->newPath('folder.yaml'));
+
+        return $this;
+    }
+}
---
+++ tests/MigrateCollectionTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests;
+
+use Tests\TestCase;
+use Statamic\Facades\YAML;
+use Statamic\Migrator\Migrators\FieldsetMigrator;
+use Tests\Console\Foundation\InteractsWithConsole;
+
+class MigrateCollectionTest extends TestCase
+{
+    protected function path($append = null)
+    {
+        return collect([base_path('content/collections'), $append])->filter()->implode('/');
+    }
+
+    /** @test */
+    function it_can_migrate_a_collection()
+    {
+        $this->files->copyDirectory(__DIR__.'/Fixtures/site/content/collections/blog', $this->path('blog'));
+
+        $this->assertFileExists($this->path('blog/folder.yaml'));
+        $this->assertFileNotExists($this->path('blog.yaml'));
+
+        $this->artisan('statamic:migrate:collection', ['handle' => 'blog']);
+
+        $this->assertFileNotExists($this->path('blog/folder.yaml'));
+        $this->assertFileExists($this->path('blog.yaml'));
+    }
+}
