13b2b62a60105ecafc1994ccc0a7262c9c0571cf
Jesse Leite <jesseleite@gmail.com>
2019-10-22 23:21:41 -0400

    Settings migration top level handling (WIP).

--- src/SettingsMigrator.php
+++ src/SettingsMigrator.php
@@ -9,6 +9,33 @@ class SettingsMigrator extends Migrator
      */
     public function migrate()
     {
-        //
+        if ($this->handle) {
+            return $this->migrateSingle();
+        }
+
+        $this
+            ->migrateAssets()
+            ->migrateCaching()
+            ->migrateCp()
+            ->migrateDebug()
+            ->migrateEmail()
+            ->migrateRoutes()
+            ->migrateRoutes()
+            ->migrateSearch()
+            ->migrateSystem()
+            ->migrateTheming()
+            ->migrateUsers();
+    }
+
+    /**
+     * Perform migration on single settings file.
+     *
+     * @return $this
+     */
+    protected function migrateSingle()
+    {
+        $migrateMethod = 'migrate' . ucfirst($this->handle);
+
+        return $this->{$migrateMethod}();
     }
 }
