cf73396aa43f9d5d7e20adc7249de5a3f4c9998a
Jesse Leite <jesseleite@gmail.com>
2019-12-24 19:07:33 -0500

    Prevent long requests from affecting user experience.

--- src/Concerns/SubmitsStats.php
+++ src/Concerns/SubmitsStats.php
@@ -18,11 +18,11 @@ trait SubmitsStats
         try {
             $stats['command'] = str_replace('statamic:', '', $stats['command']);
 
-            $ip = trim(Zttp::get('https://icanhazip.com')->body());
+            $ip = trim(Zttp::timeout(1.5)->get('https://icanhazip.com')->body());
             $ipValidator = Validator::make(['ip' => $ip], ['ip' => 'required|ip']);
 
             if ($ipValidator->passes()) {
-                Zttp::post('https://outpost.statamic.com/v3/migrator-stats', array_merge([
+                Zttp::timeout(1.5)->post('https://outpost.statamic.com/v3/migrator-stats', array_merge([
                     'app' => md5($ip . base_path())
                 ], $stats));
             }
