9926039ed3cfe53761a9ed0cf4a5aa3c4004c40c
Jesse Leite <jesseleite@gmail.com>
2020-05-08 18:51:09 -0400

    Add `MigratesLocalizedContent` trait.

---
+++ src/Concerns/MigratesLocalizedContent.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Statamic\Migrator\Concerns;
+
+use Statamic\Migrator\ContentMigrator;
+use Statamic\Support\Str;
+
+trait MigratesLocalizedContent
+{
+    use GetsSettings;
+
+    /**
+     * Check if site has multiple locales.
+     *
+     * @return bool
+     */
+    protected function isMultisite()
+    {
+        return $this->getLocales()->count() > 1;
+    }
+
+    /**
+     * Get locales.
+     *
+     * @return \Illuminate\Support\Collection
+     */
+    protected function getLocales()
+    {
+        return collect($this->getSetting('system.locales', []));
+    }
+
+    /**
+     * Get locale keys.
+     *
+     * @return \Illuminate\Support\Collection
+     */
+    protected function getLocaleKeys()
+    {
+        return $this->getLocales()->keys();
+    }
+
+    /**
+     * Get migrated site keys.
+     *
+     * @return \Illuminate\Support\Collection
+     */
+    protected function getMigratedSiteKeys()
+    {
+        $sites = $this->getLocaleKeys();
+
+        return $sites->put(0, 'default');
+    }
+
+    /**
+     * Migrate site and get filename.
+     *
+     * @param string $relativePath
+     * @return array
+     */
+    protected function migrateSiteAndGetFilename($relativePath)
+    {
+        $parts = collect(explode('/', $relativePath));
+
+        $locale = $parts->shift();
+
+        if (! $this->getLocaleKeys()->contains($locale)) {
+            return ['default', $relativePath];
+        }
+
+        $filename = $parts->implode('/');
+
+        return [$locale, $filename];
+    }
+}
