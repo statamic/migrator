3155eef8087fe6eff2812829cd8a04c66b6f75eb
Jesse Leite <jesseleite@gmail.com>
2020-05-21 21:02:14 -0400

    Add final localized pages migration implementation.

--- src/PagesMigrator.php
+++ src/PagesMigrator.php
@@ -10,9 +10,12 @@ class PagesMigrator extends Migrator
 {
     use Concerns\GetsSettings,
         Concerns\MigratesContent,
+        Concerns\MigratesLocalizedContent,
         Concerns\MigratesFile;
 
+    protected $sites = [];
     protected $entries = [];
+    protected $localizedEntries = [];
     protected $structure = [];
     protected $usedBlueprints = [];
 
@@ -60,6 +63,8 @@ class PagesMigrator extends Migrator
      */
     protected function parseTree()
     {
+        $this->sites = $this->getMigratedSiteKeys()->all();
+
         $this->parsePageFolder($this->sitePath('content/pages'));
 
         return $this;
@@ -95,7 +100,34 @@ class PagesMigrator extends Migrator
 
         data_set($this->structure, $key, $data);
 
+        if ($this->isMultisite()) {
+            $this->parseLocalizedPagesInFolder($folder, $page);
+        }
+
         return $data;
+    }
+
+    /**
+     * Parse localized pages in folder.
+     *
+     * @param string $folder
+     * @param array $pageOrigin
+     */
+    protected function parseLocalizedPagesInFolder($folder, $pageOrigin)
+    {
+        $this->getLocalizedPagesInFolder($folder)
+            ->map(function ($page) use ($pageOrigin) {
+                return array_merge($page, [
+                    'origin' => $pageOrigin['id'],
+                    'id' => Str::uuid(),
+                    'slug' => $page['slug'] ?? $pageOrigin['slug'],
+                    'fieldset' => $pageOrigin['fieldset'],
+                ]);
+            })
+            ->each(function ($page, $site) {
+                $this->localizedEntries[$site][] = $page;
+                $this->usedBlueprints[] = $page['fieldset'] ?? null;
+            });
     }
 
     /**
@@ -117,6 +149,31 @@ class PagesMigrator extends Migrator
         }
 
         return YAML::parse($page->getContents());
+    }
+
+    /**
+     * Get localized pages in folder.
+     *
+     * @param string $folder
+     * @return \Illuminate\Support\Collection
+     */
+    protected function getLocalizedPagesInFolder($folder)
+    {
+        return collect($this->files->files($folder))
+            ->keyBy
+            ->getFilenameWithoutExtension()
+            ->filter(function ($file, $filename) {
+                return Str::endsWith($filename, '.index');
+            })
+            ->keyBy(function ($file, $filename) {
+                return explode('.', $filename)[0];
+            })
+            ->filter(function ($file, $site) {
+                return in_array($site, $this->sites);
+            })
+            ->map(function ($page) {
+                return YAML::parse($page->getContents());
+            });
     }
 
     /**
@@ -204,9 +261,20 @@ class PagesMigrator extends Migrator
         collect($this->entries)->each(function ($entry) {
             $this->saveMigratedWithYamlFrontMatter(
                 $this->migrateContent($entry, $this->getPageFieldset($entry)),
-                $this->generateEntryPath($entry)
+                $this->generateEntryPath($entry, $this->isMultisite() ? 'default' : null)
             );
         });
+
+        if ($this->isMultisite()) {
+            collect($this->localizedEntries)->each(function ($siteEntries, $site) {
+                collect($siteEntries)->each(function ($entry) use ($site) {
+                    $this->saveMigratedWithYamlFrontMatter(
+                        $this->migrateContent($entry, $this->getPageFieldset($entry)),
+                        $this->generateEntryPath($entry, $site)
+                    );
+                });
+            });
+        }
 
         return $this;
     }
@@ -228,19 +296,24 @@ class PagesMigrator extends Migrator
      * Generate entry path.
      *
      * @param array $entry
+     * @param string|null $site
      * @param int $number
      * @return string
      */
-    protected function generateEntryPath($entry, $number = 1)
+    protected function generateEntryPath($entry, $site = null, $number = 1)
     {
         $appended = $number > 1
             ? "-{$number}"
             : null;
 
-        $path = $this->newPath("{$entry['slug']}{$appended}.md");
+        $subFolder = $site
+            ? "{$site}/"
+            : null;
+
+        $path = $this->newPath("{$subFolder}{$entry['slug']}{$appended}.md");
 
         if ($this->files->exists($path)) {
-            return $this->generateEntryPath($entry, ++$number);
+            return $this->generateEntryPath($entry, $site, ++$number);
         }
 
         return $path;
