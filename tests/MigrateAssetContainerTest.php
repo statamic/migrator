20c4e082065d1ce892b25ed6353e320236147e46
Jesse Leite <jesseleite@gmail.com>
2025-01-23 10:02:17 -0500

    Update normalization methods.

--- tests/MigrateAssetContainerTest.php
+++ tests/MigrateAssetContainerTest.php
@@ -788,12 +788,22 @@ EOT;
         ],
 EOT;
 
+        // Up through Larvavel 11.39
+        $variants[] = <<<'EOT'
+        'local' => [
+            'driver' => 'local',
+            'root' => storage_path('app'),
+            'throw' => false,
+        ],
+        EOT;
+
         // Current version
         $current = <<<'EOT'
         'local' => [
             'driver' => 'local',
             'root' => storage_path('app'),
             'throw' => false,
+            'report' => false,
         ],
 EOT;
 
@@ -821,6 +831,17 @@ EOT;
         ],
 EOT;
 
+        // Up through Larvavel 11.39
+        $variants[] = <<<'EOT'
+        'public' => [
+            'driver' => 'local',
+            'root' => storage_path('app/public'),
+            'url' => env('APP_URL').'/storage',
+            'visibility' => 'public',
+            'throw' => false,
+        ],
+        EOT;
+
         // Current version
         $current = <<<'EOT'
         'public' => [
@@ -829,6 +850,7 @@ EOT;
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
             'throw' => false,
+            'report' => false,
         ],
 EOT;
 
@@ -873,6 +895,21 @@ EOT;
         ],
 EOT;
 
+        // Up through Larvavel 11.39
+        $variants[] = <<<'EOT'
+        's3' => [
+            'driver' => 's3',
+            'key' => env('AWS_ACCESS_KEY_ID'),
+            'secret' => env('AWS_SECRET_ACCESS_KEY'),
+            'region' => env('AWS_DEFAULT_REGION'),
+            'bucket' => env('AWS_BUCKET'),
+            'url' => env('AWS_URL'),
+            'endpoint' => env('AWS_ENDPOINT'),
+            'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
+            'throw' => false,
+        ],
+EOT;
+
         // Current version
         $current = <<<'EOT'
         's3' => [
@@ -885,6 +922,7 @@ EOT;
             'endpoint' => env('AWS_ENDPOINT'),
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
             'throw' => false,
+            'report' => false,
         ],
 EOT;
 
