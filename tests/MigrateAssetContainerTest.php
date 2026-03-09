2cee95106b99cbed78e3b84149cb365dec0533e2
Duncan McClean <duncan@duncanmcclean.com>
2026-03-09 10:16:46 +0000

    wip

--- tests/MigrateAssetContainerTest.php
+++ tests/MigrateAssetContainerTest.php
@@ -309,7 +309,7 @@ EOT
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'url' => rtrim((string) env('APP_URL'), '/').'/storage',
             'visibility' => 'public',
             'throw' => false,
             'report' => false,
